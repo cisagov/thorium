1c877a3cca1d306e33f975e4a5838c1110e68043
Connor Aubry <caubry@sandia.gov>
2026-08-10 08:42:56 -0500

    fix(ui): Improved UI performance through caching
    Adding caching to UI. Default of 30 days cache on all js, css. Index is
    not cached. React generates unique bundles by default -- upgrades to new
    thorium versions will not render improperly on user computers.

--- api/src/routes/ui.rs
+++ api/src/routes/ui.rs
@@ -1,6 +1,10 @@
 use axum::Router;
-use axum::routing::get_service;
-use tower_http::services::{ServeDir, ServeFile};
+use http::{HeaderValue, header};
+use tower::ServiceBuilder;
+use tower_http::{
+    services::{ServeDir, ServeFile},
+    set_header::SetResponseHeaderLayer,
+};
 
 use crate::utils::AppState;
 
@@ -18,21 +22,59 @@ use crate::utils::AppState;
 pub fn mount(router: Router<AppState>) -> Router<AppState> {
     // create a new router for ui routes
     let ui_router = Router::new()
-        .nest_service("/assets", get_service(ServeDir::new("./ui/assets")))
+        .nest_service(
+            "/assets",
+            ServiceBuilder::new()
+                .layer(SetResponseHeaderLayer::overriding(
+                    header::CACHE_CONTROL,
+                    //30 day cache limit. Can increase to a year (31536000)
+                    //everything in assets/ is fingerprinted; new versions will
+                    //have different fingerprinted bundles
+                    HeaderValue::from_static("public, max-age=2592000, immutable"),
+                ))
+                .service(ServeDir::new("./ui/assets")),
+        )
         .nest_service(
             "/thorium.ico",
-            get_service(ServeFile::new("./ui/thorium.ico")),
+            ServiceBuilder::new()
+                .layer(SetResponseHeaderLayer::overriding(
+                    header::CACHE_CONTROL,
+                    //7 day cache limit
+                    HeaderValue::from_static("public, max-age=604800"),
+                ))
+                .service(ServeFile::new("./ui/thorium.ico")),
         )
         .nest_service(
             "/ferris-scientist.png",
-            get_service(ServeDir::new("./ui/ferris-scientist.png")),
+            ServiceBuilder::new()
+                .layer(SetResponseHeaderLayer::overriding(
+                    header::CACHE_CONTROL,
+                    //7 day cache limit
+                    HeaderValue::from_static("public, max-age=604800"),
+                ))
+                .service(ServeFile::new("./ui/ferris-scientist.png")),
         )
         .nest_service(
             "/manifest.json",
-            get_service(ServeFile::new("./ui/manifest.json")),
+            ServiceBuilder::new()
+                .layer(SetResponseHeaderLayer::overriding(
+                    header::CACHE_CONTROL,
+                    HeaderValue::from_static("no-cache"),
+                ))
+                .service(ServeFile::new("./ui/manifest.json")),
         )
         // always fallback to the ui index bundle for non api queries
-        .fallback(get_service(ServeFile::new("./ui/index.html")));
+        .fallback_service(
+            ServiceBuilder::new()
+                .layer(SetResponseHeaderLayer::overriding(
+                    header::CACHE_CONTROL,
+                    //important: index.html is not fingerprinted. Always want
+                    //to fetch newest version of index.html, which will call
+                    //newest versions of fingerprinted bundles in assets/
+                    HeaderValue::from_static("no-cache"),
+                ))
+                .service(ServeFile::new("./ui/index.html")),
+        );
     // merge our ui router into our global router
     router.merge(ui_router)
 }
